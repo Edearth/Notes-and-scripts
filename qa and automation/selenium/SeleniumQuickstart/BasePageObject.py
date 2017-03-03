096eda5b1fac82a6deb3142f5425627fa6d7f9e8
Juan Antonio Nepormoseno Rosales <nepormoseno1992@gmail.com>
2017-03-03 12:15:20 +0100

    Actualizado el esquema de la página base

---
+++ qa and automation/selenium/SeleniumQuickstart/BasePageObject.py
@@ -0,0 +1,52 @@
+#class BasePageObject()
+# CONSTRUCTOR
+# public BaseScreenObject(ProcessExecution pe) {
+#	this.pe = pe
+#	try {
+#			driver = TestSettings.getInstance().getCurrentDriver();
+#		} catch (Exception e) {
+#			log.severe(TestSettings.getStackTrace(e));
+#		}
+#	}
+#
+# METHODS
+# > get element
+# protected WebElement getElement(By by)
+# protected List<MobileElement> getElements(By by)
+# protected WebElement getElementWithoutWaiting(By by)
+# protected WebElement getElementWithExplicitTimeout(By by, int timeoutSeconds)
+# protected List<WebElement> getChildrenOfElement(WebElement parent, String typeOfChildren)
+# protected List<MobileElement> getChildrenOfElement(By parent, String typeOfChildren)
+# protected List<MobileElement> getChildrenOfElement(String xpath)
+# > click
+# protected void click(By by)
+# protected void click(WebElement element)
+# protected void clickElementIfExists(By by)
+# protected void clickWithoutWaiting(By by)
+# > fill text
+# protected void fillText(By by, String text)
+# protected void fillText(WebElement element, String text)
+# protected void fillTextWithoutWaiting(By by, String text)
+# public void selectValueOfPickerWheel(By by, String date)
+# > get value
+# protected String getValue(By by)
+# protected String getValue(WebElement element)
+# protected String getValueWithoutWaiting(By by)
+# protected String getValueWithoutWaiting(WebElement element)
+# > text
+# protected String getElementText(By by)
+# protected String getElementText(WebElement we)
+# protected Dimension getElementSize(WebElement we)
+# protected Point getElementLocation(WebElement we)
+# > waits
+# protected void waitForElementToLoad(By by)
+# protected void waitForElementToDisappear(By by)
+# protected void wait(int seconds) throws InterruptedException
+# ** faltan waits **
+# > condiciones
+# protected boolean elementExists(By element)
+# > helpers
+# protected String getElementXPath(WebElement we)
+# public void scrollTo(By element)
+# public void scrollTo(Point location)
+# public void scrollDown()
